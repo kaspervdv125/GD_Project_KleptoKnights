4153a3614b3672f2468c9513cf54f7893d649622
alexconmar <168528779+ConmanConmar@users.noreply.github.com>
2024-05-07 12:31:25 +0200

    Implemented timer and CoinScore logic

--- KleptoKnights/Assets/Scripts/Timer.cs
+++ KleptoKnights/Assets/Scripts/Timer.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
+    [SerializeField]
+    private float timerTarget = 30f;
+    [SerializeField]
+    private float coinThreshold = 5; 
+
+    private int coinCounter;
+
+    private bool endGame;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,12 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timerTarget -= Time.deltaTime;
+
+        if(timerTarget < 0 | coinThreshold <= coinCounter )
+        {
+            endGame = true;
+        }
+
     }
 }
