6b38284355831183db7d9f1dadc58d33b3b66835
kaspervdv125 <kasper125.vdv@gmail.com>
2024-05-21 09:58:54 +0200

    Attack Cooldown UI

--- KleptoKnights/Assets/Scripts/UI.cs
+++ KleptoKnights/Assets/Scripts/UI.cs
@@ -7,15 +7,11 @@ using UnityEngine.UI;
 public class UI : MonoBehaviour
 {
     public Slider HealthBar;
-    public TMP_Text ScoreField;
-    private GameObject TeamScoreBox;
 
     // Start is called before the first frame update
     void Start()
     {
         HealthBar.maxValue = GetComponent<EnemyHealth>().maxHealth;
         HealthBar.value = HealthBar.maxValue;
-
-
     }
 }
