748c8eb8cacd417152d491bff050a8da59fd74cb
kaspervdv125 <kasper125.vdv@gmail.com>
2024-05-27 14:03:43 +0200

    Made start screen load correct scenes
    The actual game scenes are now Game1v1 and Game2v2

--- KleptoKnights/Assets/Scripts/StartScreenScript.cs
+++ KleptoKnights/Assets/Scripts/StartScreenScript.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Net.Sockets;
 
 public class StartScreenScript : MonoBehaviour
 {
     public GameObject playScreen;
     public GameObject optionsPanel;
+    public GameObject selectPlayMode;
 
     public Button playButton;
     public Button exitButton;
     public Button optionsButton;
-    public Button backButton;
+    public Button optionsBackButton;
+    public Button oneVOneButton;
+    public Button twoVTwoButton;
+    public Button playModeBackButton;
+
     public Slider soundSlider;
     public Slider musicSlider;
 
@@ -22,23 +28,41 @@ public class StartScreenScript : MonoBehaviour
     void Start()
     {
         optionsPanel.SetActive(false);
+        selectPlayMode.SetActive(false);
+
         playButton.onClick.AddListener(PlayGame);
         exitButton.onClick.AddListener(ExitGame);
         optionsButton.onClick.AddListener(OpenOptions);
-        backButton.onClick.AddListener(CloseOptions);
+        optionsBackButton.onClick.AddListener(BackToMainMenu);
+        oneVOneButton.onClick.AddListener(StartGameOneVOne);
+        twoVTwoButton.onClick.AddListener(StartGameTwoVTwo);
+        playModeBackButton.onClick.AddListener(BackToMainMenu);
 
         soundSlider.onValueChanged.AddListener(ChangeSound);
         musicSlider.onValueChanged.AddListener(ChangeMusic);
     }
 
+    void StartGameTwoVTwo()
+    {
+        SceneManager.LoadScene("Game2v2");
+    }
+
+    void StartGameOneVOne()
+    {
+        SceneManager.LoadScene("Game1v1");
+    }
+
     void PlayGame()
     {
         Debug.Log("Play Game");
+        playScreen.SetActive(false);
+        selectPlayMode.SetActive(true);
     }
 
     void ExitGame()
     {
         Debug.Log("Exit Game");
+        Application.Quit();
     }
 
     void OpenOptions()
@@ -47,9 +71,10 @@ public class StartScreenScript : MonoBehaviour
         optionsPanel.SetActive(true);
     }
 
-    void CloseOptions()
+    void BackToMainMenu()
     {
         optionsPanel.SetActive(false);
+        selectPlayMode.SetActive(false);
         playScreen.SetActive(true);
     }
 
